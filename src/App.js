28a013b6288172cb4638ca9b3c68cd3353755ca7
Blake Coffee <blake.coffee8@gmail.com>
2023-11-05 12:31:42 -0600

    Added navigate functionality as well as authorization context within the pages so that users stay logged in and their token is stored

--- src/App.js
+++ src/App.js
@@ -1,120 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
-import UserProfile from './components/UserProfile';
 import Home from './components/Home';
-import UserSettings from './components/UserSettings'
-import Footer from './components/Footer';
+import UserSettings from './components/UserSettings';
+import UserProfile from './components/UserProfile';
 import BusinessSignIn from './components/BusinessSignIn';
 import BusinessProfile from './components/BusinessProfile';
 import SearchScreen from './components/SearchScreen';
-
+import Footer from './components/Footer';
+import { AuthProvider } from './AuthContext'; // Import AuthProvider
 
 function App() {
-  const [activeTab, setActiveTab] = useState('home');
-  const [isProfileVisible, setIsProfileVisible] = useState(false); // New state variable
-  const [isHomeVisible, setIsHomeVisible] = useState(false);
-  const [isUserSignedIn, setIsUserSignedIn] = useState(false);
-  const [isBusinessSignedIn, setIsBusinessSignedIn] = useState(false);
-  const [isSignedUp, setIsSignedUp] = useState(false);
-  const [user, setUser] = useState(null);
-  const [business, setBusiness] = useState(null);
-
-   // State for component transition
-   const [isEntering, setIsEntering] = useState(false);
-   const [isExiting, setIsExiting] = useState(false);
-
-  const handleTabClick = (tab) => {
-    setIsExiting(true); // Start the exit transition
-    setTimeout(() => {
-      setActiveTab(tab); // Change the tab after the exit transition
-      setIsEntering(true); // Start the enter transition
-    }, 300); // Adjust the duration to match your CSS transition duration
-  };
-
-  const handleUserSignUp = (user) => {
-    setUser(user);
-    setActiveTab('userprofile');
-  };
-
-  const handleUserSignIn = () => {
-    console.log(isUserSignedIn);
-    setActiveTab('searchscreen');
-    setIsUserSignedIn(true);
-    console.log(isUserSignedIn);
-  };
-
-
-  const handleUserSignOut = () => {
-    setUser(null);
-    setActiveTab('signin');
-    setIsUserSignedIn(false);
-  };
-
-    const handleBusinessSignOut = () => {
-    setBusiness(null);
-    setActiveTab('signin');
-    setIsBusinessSignedIn(false);
-  };
-
-  // New function to toggle the visibility of the user profile
-  const handleUserProfileButtonClick = () => {
-    setActiveTab('userprofile');
-    // setIsProfileVisible(!isProfileVisible);
-  };
-
-  const handleBusinessProfileButtonClick = () => {
-    setActiveTab('businessprofile');
-    // setIsProfileVisible(!isProfileVisible);
-  };
-
-  const handleHomeButtonClick = () => {
-    setActiveTab('home');
-    // setIsProfileVisible(!isProfileVisible);
-  };
- 
-  const handleSettingsButtonClick = () => {
-    setActiveTab('usersettings');
-    // setIsProfileVisible(!isProfileVisible);
-  };
-
-  const handleBusinessSignIn = () => {
-    setActiveTab('searchscreen'); // Set the active tab to 'businesssignin'
-    setIsBusinessSignedIn(true); // Set isSignedIn to true
-  }
-
   return (
-    <div className="App">
-      <Header 
-        onUserProfileButtonClick={handleUserProfileButtonClick}
-        onBusinessProfileButtonClick={handleBusinessProfileButtonClick}
-        onHomeButtonClick={handleHomeButtonClick} 
-        onSettingsButtonClick={handleSettingsButtonClick}
-        onSignIn={() => setActiveTab('signin')}
-        onSignUp={() => setActiveTab('signup')}
-        onBusinessSignIn={() => setActiveTab('businesssignin')}
-        isUserSignedIn={isUserSignedIn}
-        isBusinessSignedIn={isBusinessSignedIn}
-      />
-      {activeTab === 'signup' && <SignUp onSignUp={handleUserSignUp} />}
-      {activeTab === 'signin' && <SignIn onUserSignIn={handleUserSignIn} />}
-      {activeTab === 'searchscreen' && <SearchScreen onSignIn={handleUserSignIn} />}
-      {!isUserSignedIn && !isBusinessSignedIn && activeTab === 'home' && <Home />}
-      {activeTab === 'businesssignin' && <BusinessSignIn onBusinessSignIn={handleBusinessSignIn} />}
-      {activeTab === 'usersettings' && <UserSettings onSignIn={handleUserSignIn} />}
-      {activeTab === 'userprofile' && (
-        <UserProfile user={user} onSignOut={handleUserSignOut} />
-      )}
-      {activeTab === 'businessprofile' && (
-        <BusinessProfile business={business} onSignOut={handleBusinessSignOut} />
-      )}
-      <div>
-        <Footer />
-      </div>
-    </div>
+    <AuthProvider> {/* Wrap your entire app with AuthProvider */}
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/businesssignin" element={<BusinessSignIn />} />
+            <Route path="/usersettings" element={<UserSettings />} />
+            <Route path="/userprofile" element={<UserProfile />} />
+            <Route path="/businessprofile" element={<BusinessProfile />} />
+            <Route path="/searchscreen" element={<SearchScreen />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../AuthContext';
 import './SignIn.css';
 
-function SignIn({onUserSignIn, onBusinessSignIn }) {
+function SignIn() {
+  const navigate = useNavigate();
+  const { signIn } = useAuth(); // Get the signIn function from the context
   const [showComponent, setShowComponent] = useState(false);
   const [formData, setFormData] = useState({
     Email: '',
     Password: '',
   });
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+  
     console.log('Sending sign-in request:', formData);
-
-    // Send a POST request to your backend to handle user authentication
-    // You should replace 'YOUR_BACKEND_API_URL' with your actual API endpoint.
+  
     try {
       const response = await fetch('http://localhost:4444/users/login', {
         method: 'POST',
@@ -24,40 +25,39 @@ function SignIn({onUserSignIn, onBusinessSignIn }) {
         },
         body: JSON.stringify(formData),
       });
-    
+  
       if (response.ok) {
-        console.log('User authenticated successfully');
-        const responseData = await response.json(); // Parse the JSON response
+        const responseData = await response.json();
         const user = { ...responseData.data, authenticated: true };
         localStorage.setItem('user', JSON.stringify(user));
-        onUserSignIn(formData);
+        console.log('User Authentication Successful');
+        signIn(); // Call the signIn function to update the context
+        navigate('/searchscreen'); // Redirect to the protected route
       } else {
         console.error('User authentication failed');
       }
     } catch (error) {
       console.error('Error:', error);
     }
-  };
-
+  }
 
   useEffect(() => {
-    setShowComponent(true); // Show the component immediately
+    setShowComponent(true);
 
-    // Add the transition effect after a short delay
     const transitionDelay = setTimeout(() => {
-      setShowComponent(false); // Remove the transition effect class
-    }, 100); // You can adjust the duration as needed
+      setShowComponent(false);
+    }, 100);
 
     return () => clearTimeout(transitionDelay);
   }, []);
 
   return (
-    <div className={`signin ${showComponent ? 'visible' : 'transition-effect'}`}>    
-      <div className="sign-in-container" >
-      <h2 className="title">Login</h2>
-      <h3 className="subtitle">Please sign in to continue</h3>
+    <div className={`signin ${showComponent ? 'visible' : 'transition-effect'}`}>
+      <div className="sign-in-container">
+        <h2 className="title">Login</h2>
+        <h3 className="subtitle">Please sign in to continue</h3>
         <form onSubmit={handleSubmit}>
-          <input 
+          <input
             type="email"
             placeholder=" Email"
             value={formData.Email}
@@ -73,25 +73,22 @@ function SignIn({onUserSignIn, onBusinessSignIn }) {
             required
             className="custom-input"
           />
-
-          <button 
-            type="submit" 
-            className="custom-button"
-            >
+          <button type="submit" className="custom-button">
             Sign In
-          </button>          
-
-        </form>
-          <button 
-            type="nav" 
-            className="alt-login"
-            onClick={onBusinessSignIn}
-            >
-            Are you a merchant? Sign in here
           </button>
+        </form>
+        <button
+          type="button"
+          className="alt-login"
+          onClick={() => {
+            // Handle business sign-in or navigation here
+            navigate('/businesssignin');
+          }}
+        >
+          Are you a merchant? Sign in here
+        </button>
       </div>
     </div>
-
   );
 }
 
