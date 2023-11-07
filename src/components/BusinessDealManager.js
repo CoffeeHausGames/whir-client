7b6bcf83d01a950ef55e85f02ab154d59af59201
Blake Coffee <blakecoffee@Blakes-MacBook-Pro-3.local>
2023-11-06 20:00:49 -0600

    added the deals page with a deal browser

--- src/components/BusinessDealManager.js
+++ src/components/BusinessDealManager.js
@@ -1,28 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './BusinessDealManager.css';
+import DealBox from './DealBox';
 
 const BusinessDealManager = () => {
-  const [showForm, setShowForm] = useState(false);
-  const [dealName, setDealName] = useState('');
-  const [dealStart, setDealStart] = useState('');
-  const [dealEnd, setDealEnd] = useState('');
-  const [dayOfWeek, setDayOfWeek] = useState('');
-  const [repeating, setRepeating] = useState('');
-  const [description, setDescription] = useState('');
+ const [showForm, setShowForm] = useState(false);
+ const [dealName, setDealName] = useState('');
+ const [dealStart, setDealStart] = useState('');
+ const [dealEnd, setDealEnd] = useState('');
+ const [dayOfWeek, setDayOfWeek] = useState('');
+ const [repeating, setRepeating] = useState('');
+ const [description, setDescription] = useState('');
 
-  const handleSaveDeal = () => {
-    // Use an API call to save the deal information to the backend
-    // You can use libraries like axios to make the API call
-    // Example: axios.post('/api/deals', { name: dealName, start_time: dealStart, ... })
-  };
+ // Insert the deals directly into the deals state
+ const [deals, setDeals] = useState([
+   { id: 1, name: 'Deal 1', description: 'Description 1' },
+   { id: 2, name: 'Deal 2', description: 'Description 2' },
+   // Add more deals here...
+ ]);
 
-  const handleAddDeal = () => {
-    setShowForm(true);
-  };
+ const [showDealBox, setShowDealBox] = useState(true);
 
-  const handleCancel = () => {
-    setShowForm(false);
-  };
+ useEffect(() => {
+   fetch('APICODEHERE')
+     .then(response => response.json())
+     .then(data => {
+       setDeals(data);
+     })
+     .catch(error => {
+       console.error('Error fetching deals:', error);
+     });
+ }, []);
+
+ const handleSaveDeal = () => {
+   // Use an API call to save the deal information to the backend
+   // You can use libraries like axios to make the API call
+   // Example: axios.post('/api/deals', { name: dealName, start_time: dealStart, ... })
+ };
+
+ const handleAddDeal = () => {
+   setShowDealBox(false);
+   setShowForm(true);
+ };
+
+ const handleCancel = () => {
+   setShowDealBox(true);
+   setShowForm(false);
+ };
+
 
   return (
     <div className="business-deal-manager">
@@ -97,16 +121,21 @@ const BusinessDealManager = () => {
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
+          <div className="button-container">
+            <button className="save save-deal-button" onClick={handleSaveDeal}>
+              Save Deal
+            </button>
 
-          <button className="save save-deal-button" onClick={handleSaveDeal}>
-            Save Deal
-          </button>
+            <button className="cancel cancel-deal-button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
 
-          <button className="cancel cancel-deal-button" onClick={handleCancel}>
-            Cancel
-          </button>
         </>
       )}
+         {showDealBox && (
+           <DealBox />
+         )}
     </div>
   );
 };
