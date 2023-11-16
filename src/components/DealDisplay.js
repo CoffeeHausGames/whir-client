8c163ed60427794f5bd9e6fc50541d191e8a8ee1
Blake Coffee <blake.coffee8@gmail.com>
2023-11-15 18:35:06 -0600

    deal click and relocation is working

--- src/components/DealDisplay.js
+++ src/components/DealDisplay.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './DealDisplay.css';
-import MapComponent from './Map';
 
 const DealDisplay = ({ setSelectedBusinessLocation }) => {
   const [businesses, setBusinesses] = useState([]);
   const [selectedBusiness, setSelectedBusiness] = useState(null);
-  const [selectedBusinessLocationState, setSelectedBusinessLocationState] = useState(null);
 
   useEffect(() => {
     const fetchBusinesses = () => {
@@ -59,40 +57,38 @@ const handleDealClick = (business) => {
   console.log(business);
   const businessLocation = getBusinessLocation(business);
   if (businessLocation) {
-    setSelectedBusinessLocationState({ latitude: businessLocation.latitude, longitude: businessLocation.longitude });
-    setSelectedBusinessLocation(businessLocation);
+    setSelectedBusinessLocation(businessLocation); // Pass the selected location to the parent
     setSelectedBusiness(business); // Set the selected business
   } else {
     console.error(`Invalid location data for business: ${business.id}`);
   }
- };
+};
 
-  return (
-    <div className="deal-display-container">
-      <h2>Current Deals</h2>
-      {businesses.map((business) => (
-        <div key={business.business_name}>
-          <h3>{business.business_name}</h3>
-          {business.deal && business.deal.length > 0 ? (
-            <ul>
-              {business.deal.map((deal) => (
-                <button onClick={() => handleDealClick(business)}>
-                  <li key={deal.id}>
-                    <strong>{deal.name}</strong>: {deal.description}
-                    <br />
-                    Start Date: {deal.start_date}, End Date: {deal.end_date}  
-                  </li>
-                </button>
-              ))}
-            </ul>
-          ) : (
-            <p>No deals available for this business</p>
-          )}
-        </div>
-      ))}
-      {selectedBusinessLocationState && <MapComponent selectedBusinessLocation={selectedBusinessLocationState} />}
-    </div>
-  );
+return (
+  <div className="deal-display-container">
+    <h2>Current Deals</h2>
+    {businesses.map((business) => (
+      <div key={business.business_name}>
+        <h3>{business.business_name}</h3>
+        {business.deal && business.deal.length > 0 ? (
+          <ul>
+            {business.deal.map((deal) => (
+              <button onClick={() => handleDealClick(business)} key={deal.id}>
+                <li>
+                  <strong>{deal.name}</strong>: {deal.description}
+                  <br />
+                  Start Date: {deal.start_date}, End Date: {deal.end_date}
+                </li>
+              </button>
+            ))}
+          </ul>
+        ) : (
+          <p>No deals available for this business</p>
+        )}
+      </div>
+    ))}
+  </div>
+);
 };
 
 export default DealDisplay;
